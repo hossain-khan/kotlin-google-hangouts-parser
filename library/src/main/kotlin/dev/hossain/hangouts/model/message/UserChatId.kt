aa4351c0aecb9dba2aa015036d58e6d5e9d8cfbe
Hossain Khan <amardeshbd@gmail.com>
2020-06-28 16:43:53 -0400

    [ADDED] Info about GAIA ID

--- library/src/main/kotlin/dev/hossain/hangouts/model/message/UserChatId.kt
+++ library/src/main/kotlin/dev/hossain/hangouts/model/message/UserChatId.kt
@@ -12,6 +12,14 @@ import com.squareup.moshi.JsonClass
  */
 @JsonClass(generateAdapter = true)
 data class UserChatId(
+    /**
+     * GAIA stands for Google Accounts and ID Administration
+     *
+     * The Gaia ID may be obtained with the People API, by requesting the metadata in the personFields.
+     *
+     * @see <a href="https://developers.google.com/people/">People API</a>
+     * @see <a href="https://developers.google.com/people/api/rest/v1/people/get">People API Explorer</a>
+     */
     val gaia_id: String,
     val chat_id: String
 )
