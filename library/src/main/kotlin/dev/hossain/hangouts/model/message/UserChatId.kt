3819486662e8388f46af7587145e950b11ccdec4
Hossain Khan <amardeshbd@gmail.com>
2020-06-28 16:48:37 -0400

    [ADDED] More clues about GAIA

--- library/src/main/kotlin/dev/hossain/hangouts/model/message/UserChatId.kt
+++ library/src/main/kotlin/dev/hossain/hangouts/model/message/UserChatId.kt
@@ -5,8 +5,8 @@ import com.squareup.moshi.JsonClass
 /**
  * ```
  * {
- *  "gaia_id": "11003566",
- *  "chat_id": "11003566"
+ *  "gaia_id": "110...66",
+ *  "chat_id": "110...566"
  * }
  * ```
  */
@@ -16,6 +16,9 @@ data class UserChatId(
      * GAIA stands for Google Accounts and ID Administration
      *
      * The Gaia ID may be obtained with the People API, by requesting the metadata in the personFields.
+     *
+     * > TIP: How to find your own GAIA?
+     * > Go to https://get.google.com/albumarchive and the 21 numbers that reveal itself in address bar.
      *
      * @see <a href="https://developers.google.com/people/">People API</a>
      * @see <a href="https://developers.google.com/people/api/rest/v1/people/get">People API Explorer</a>
