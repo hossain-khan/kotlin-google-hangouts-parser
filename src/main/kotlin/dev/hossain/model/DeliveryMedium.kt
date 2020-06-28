b838343374d91db162533ffaa498c8aa4c7b1e5c
Hossain Khan <amardeshbd@gmail.com>
2020-06-28 00:24:05 -0400

    [ADDED] Phone number for Google Voice Calls

--- src/main/kotlin/dev/hossain/model/DeliveryMedium.kt
+++ src/main/kotlin/dev/hossain/model/DeliveryMedium.kt
@@ -4,10 +4,14 @@ import com.squareup.moshi.JsonClass
 
 /**
  * ```
- * { "medium_type": "BABEL_MEDIUM" }
+ * {
+ *    "medium_type": "BABEL_MEDIUM" | "GOOGLE_VOICE_MEDIUM",
+ *    "self_phone": { "e164": "+1234567890" }
+ * }
  * ```
  */
 @JsonClass(generateAdapter = true)
 data class DeliveryMedium(
-    val medium_type: String
+    val medium_type: String,
+    val self_phone: Phone? = null
 )
---
+++ src/main/kotlin/dev/hossain/model/Phone.kt
@@ -0,0 +1,17 @@
+package dev.hossain.model
+
+import com.squareup.moshi.JsonClass
+
+/**
+ * ```
+ * { "e164": "+1234567890" }
+ * ```
+ */
+@JsonClass(generateAdapter = true)
+data class Phone(
+    /**
+     * - https://en.wikipedia.org/wiki/E.164
+     * - https://www.twilio.com/docs/glossary/what-e164
+     */
+    val e164: String
+)
