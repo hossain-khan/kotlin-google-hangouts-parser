8793b5f45c76fc669e94969702d8925b6d8ca5ca
Hossain Khan <amardeshbd@gmail.com>
2020-06-28 16:44:09 -0400

    [ADDED] More test code to check the stats

--- example/src/main/kotlin/dev/hossain/hangouts/example/HangoutsJsonProcessor.kt
+++ example/src/main/kotlin/dev/hossain/hangouts/example/HangoutsJsonProcessor.kt
@@ -3,6 +3,7 @@ package dev.hossain.hangouts.example
 import dev.hossain.hangouts.Parser
 import dev.hossain.hangouts.model.HangoutsDocument
 import okio.Okio
+import kotlin.system.measureTimeMillis
 
 
 fun main() {
@@ -11,22 +12,34 @@ fun main() {
 }
 
 object Processor {
+    private lateinit var hangoutsDocument: HangoutsDocument
     fun processTakeoutFile(path: String) {
         val inputStream = Processor::class.java.getResourceAsStream(path)
 
         val source = Okio.buffer(Okio.source(inputStream))
 
-        val hangoutsDocument: HangoutsDocument = Parser.parse(source)
-        println("Completed processing - got ${hangoutsDocument.conversations.size} conversations.")
-        hangoutsDocument.conversations.stream().forEach {
-            it.events.stream().forEach { event ->
-                event.chat_message?.message_content?.attachment?.stream()?.forEach { attachment ->
+        val parseTime = measureTimeMillis { hangoutsDocument = Parser.parse(source) }
+        println("Completed processing - got ${hangoutsDocument.conversations.size} conversations in ${parseTime}ms.")
+
+        var attachmentCount = 0
+        var participantsCount = 0
+        val participantIds = mutableSetOf<String>()
+        hangoutsDocument.conversations.forEach { conversationContainer ->
+            conversationContainer.conversation?.conversation?.participant_data?.let {
+                participantsCount += it.size
+                participantIds.addAll(it.map { it.id.gaia_id })
+            }
+            conversationContainer.events.forEach { event ->
+                event.chat_message?.message_content?.attachment?.forEach { attachment ->
                     if (attachment.embed_item != null) {
                         // Checks the attachments
-                        println(attachment.embed_item)
+                        attachmentCount++
                     }
                 }
             }
         }
+        println("Total attachments used: $attachmentCount")
+        println("Total participants across all conversations (non-uniqueue): $participantsCount")
+        println("Total participants across all conversations (uniqueue): ${participantIds.size}")
     }
 }
