9d0c55f290995d0a45f22451b9cac4d4c1cd7f20
Hossain Khan <amardeshbd@gmail.com>
2020-06-28 01:39:06 -0400

    [ADDED] Documentation for the ROOT doc

--- src/main/kotlin/dev/hossain/model/HangoutsDocument.kt
+++ src/main/kotlin/dev/hossain/model/HangoutsDocument.kt
@@ -1,7 +1,31 @@
 package dev.hossain.model
 
 import com.squareup.moshi.JsonClass
+import dev.hossain.model.event.Event
 
+/**
+ * This is the root JSON document for the "Hangouts.json" file received from Google Takeout.
+ *
+ * Here is redacted snapshot of the document:
+ *
+ * ```
+ * {
+ *   "conversations": [
+ *       {
+ *         "conversation": {
+ *           "conversation_id": {},
+ *           "conversation": {...},
+ *         }
+ *        "events" : [ {...}, {...} ]
+ *     },
+ *     { ... },
+ *     { ... }
+ *   ]
+ * }
+ * ```
+ * @see Conversation
+ * @see Event
+ */
 @JsonClass(generateAdapter = true)
 data class HangoutsDocument(
     val conversations: List<ConversationContainer>
